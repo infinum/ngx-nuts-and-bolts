e3222b7d4bd52a28d25cd366999b8ffa1e7e3ba9
Filip Voska <filip.voska@infinum.com>
2021-11-05 17:03:15 +0100

    updates

--- apps/ngx-nuts-and-bolts-demo/src/app/components/documentation-page/documentation-page.component.spec.ts
+++ apps/ngx-nuts-and-bolts-demo/src/app/components/documentation-page/documentation-page.component.spec.ts
@@ -1,25 +1,23 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { DocumentationPageComponent } from './documentation-page.component';
 
 describe('DocumentationPageComponent', () => {
-  let component: DocumentationPageComponent;
-  let fixture: ComponentFixture<DocumentationPageComponent>;
+	let component: DocumentationPageComponent;
+	let fixture: ComponentFixture<DocumentationPageComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ DocumentationPageComponent ]
-    })
-    .compileComponents();
-  });
+	beforeEach(async () => {
+		await TestBed.configureTestingModule({
+			declarations: [DocumentationPageComponent],
+		}).compileComponents();
+	});
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(DocumentationPageComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+	beforeEach(() => {
+		fixture = TestBed.createComponent(DocumentationPageComponent);
+		component = fixture.componentInstance;
+		fixture.detectChanges();
+	});
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+	it('should create', () => {
+		expect(component).toBeTruthy();
+	});
 });
