ab6148b96989b86dc76060b9fcbab639808ce474
Filip Voska <filip.voska@infinum.com>
2021-11-05 10:02:56 +0100

    initial commit

---
+++ apps/ngx-nuts-and-bolts-demo/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ngx-nuts-and-bolts-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'ngx-nuts-and-bolts-demo';
+}
