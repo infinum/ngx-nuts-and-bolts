6e3a6a62cc8ae76fb7a61be1b2e73836a31f2e98
Miroslav Foltýn <miroslav.foltyn64@gmail.com>
2022-01-19 17:36:31 +0100

    add testing module to index.ts

--- libs/ngx-nuts-and-bolts/src/index.ts
+++ libs/ngx-nuts-and-bolts/src/index.ts
@@ -1,2 +1,3 @@
 export * from './lib/components/component-with-loading-state/component-with-loading-state';
 export * from './lib/directives/in-view/in-view.module';
+export * from './lib/directives/in-view/in-view.testing.module';
