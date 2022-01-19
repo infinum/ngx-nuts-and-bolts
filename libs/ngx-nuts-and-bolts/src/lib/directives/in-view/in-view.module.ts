ff6924e715e2c21b1b188560b5a70471bb768e17
Miroslav Foltýn <miroslav.foltyn64@gmail.com>
2022-01-19 16:40:10 +0100

    add inview directive

---
+++ libs/ngx-nuts-and-bolts/src/lib/directives/in-view/in-view.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { InViewDirective } from './in-view.directive';
+
+@NgModule({
+	declarations: [InViewDirective],
+	imports: [CommonModule],
+	exports: [InViewDirective],
+})
+export class InViewModule { }
