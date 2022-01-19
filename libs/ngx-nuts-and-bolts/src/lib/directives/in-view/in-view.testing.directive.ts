b09e9a493b283d1f8bf43bdc232a4e7dcda908f7
Miroslav Foltýn <miroslav.foltyn64@gmail.com>
2022-01-19 16:40:10 +0100

    add inview directive

---
+++ libs/ngx-nuts-and-bolts/src/lib/directives/in-view/in-view.testing.directive.ts
@@ -0,0 +1,22 @@
+import { AfterViewInit, Directive, EventEmitter, OnDestroy, Output } from '@angular/core';
+import { InViewDirective } from './in-view.directive';
+
+// TODO: @JosipJanus needs to merge https://github.com/infinum/ngx-nuts-and-bolts/pull/10 first
+// eslint-disable-next-line @typescript-eslint/ban-types
+type ExtractPublic<T extends object> = {
+	[K in keyof T]: T[K];
+};
+
+@Directive({ selector: '[infInView]' })
+export class InViewTestingDirective implements ExtractPublic<InViewDirective>, AfterViewInit, OnDestroy {
+	@Output()
+	public inView: EventEmitter<boolean> = new EventEmitter<boolean>();
+
+	public isInView: boolean | undefined;
+
+	public ngAfterViewInit(): void {
+		this.inView.emit(true);
+	}
+
+	public ngOnDestroy = (): void => undefined;
+}
