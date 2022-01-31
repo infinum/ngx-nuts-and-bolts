149319c846f9724fbfdcbe4eb61584f68964cd2f
Josip Januš <81430017+JosipJanus@users.noreply.github.com>
2022-01-31 09:46:45 +0100

    Add fade and height animations (#30)

--- ngx-nuts-and-bolts-docs/sidebars.js
+++ ngx-nuts-and-bolts-docs/sidebars.js
@@ -26,15 +26,20 @@ const sidebars = {
 			label: 'Components',
 			items: ['loading-state/component-with-loading-state'],
 		},
+
+		{
+			type: 'category',
+			label: 'Animations',
+			items: ['animations/animations'],
+		},
 		{
 			type: 'category',
 			label: 'Utilities',
 			items: ['utilities/testing/extract-public'],
 		},
 		{
-			type: 'category',
-			label: 'Helpers',
-			items: ['table-state/table-state'],
+			type: 'doc',
+			id: 'table-state/table-state',
 		},
 	],
 
