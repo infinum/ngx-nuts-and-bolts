af81ca13c2caa01adb59ecc4a9c0cb545faf06fd
Filip Voska <filip.voska@infinum.com>
2022-02-28 15:36:37 +0100

    Reorganize files and add missing public API export for asyncData (#58)
    * Reorganize files and add missing public API export for asyncData
    
    * Reorder exports

--- libs/ngx-nuts-and-bolts/src/index.ts
+++ libs/ngx-nuts-and-bolts/src/index.ts
@@ -4,16 +4,17 @@ export * from './lib/animations/height.animation';
 
 // Testing utilities
 export * from './lib/testing/extract-public/extract-public.type';
-
-// Table state helpers
-export * from './lib/helpers/table-state/table-state.helper';
+export * from './lib/testing/async-data/async-data.helper';
 
 // Directives
 export * from './lib/directives/in-view/in-view.module';
 export * from './lib/directives/in-view/in-view.directive';
 export * from './lib/directives/in-view/in-view.testing.module';
 export * from './lib/directives/in-view/in-view.testing.directive';
-export * from './lib/directives/loading-state/loading-state';
+
+// Utilities
+export * from './lib/utilities/loading-state/loading-state';
+export * from './lib/utilities/table-state/table-state';
 
 // Pipes
 export * from './lib/pipes/enum-property/enum-property.module';
