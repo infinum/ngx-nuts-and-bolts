38efb17c8dba84fc306295e2bc568b1295c68896
Filip Voska <filip.voska@infinum.com>
2023-05-09 16:21:11 +0200

    chore: handle error events

--- libs/ngx-nuts-and-bolts/src/lib/utilities/route-config-loading/route-config-loading.ts
+++ libs/ngx-nuts-and-bolts/src/lib/utilities/route-config-loading/route-config-loading.ts
@@ -1,9 +1,12 @@
-import { RouteConfigLoadEnd, RouteConfigLoadStart, Router } from '@angular/router';
+import { NavigationError, RouteConfigLoadEnd, RouteConfigLoadStart, Router } from '@angular/router';
 import { Observable, filter, map } from 'rxjs';
 
 export function createRouteConfigLoadingObservable(router: Router): Observable<boolean> {
 	return router.events.pipe(
-		filter((event) => event instanceof RouteConfigLoadStart || event instanceof RouteConfigLoadEnd),
+		filter(
+			(event) =>
+				event instanceof RouteConfigLoadStart || event instanceof RouteConfigLoadEnd || event instanceof NavigationError
+		),
 		map((event) => event instanceof RouteConfigLoadStart)
 	);
 }
