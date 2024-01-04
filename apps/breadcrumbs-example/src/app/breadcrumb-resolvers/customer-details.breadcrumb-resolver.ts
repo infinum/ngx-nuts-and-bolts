60ce2fb9518a010819ab9b3445335f4c7fe7892e
Filip Voska <filip.voska@infinum.com>
2024-01-04 14:38:39 +0100

    remove outdated comment

--- apps/breadcrumbs-example/src/app/breadcrumb-resolvers/customer-details.breadcrumb-resolver.ts
+++ apps/breadcrumbs-example/src/app/breadcrumb-resolvers/customer-details.breadcrumb-resolver.ts
@@ -12,8 +12,5 @@ export const customerDetailsBreadcrumbResolver: ResolveFn<BreadcrumbData> = (rou
 		return 'Customer not found';
 	}
 
-	// If the component that gets loaded on this route also fetches the customer data, you will have 2 identical API calls.
-	// This should be solved/optimized in application codebase, most probably by using a data store or some other method
-	// of deduping of API calls.
 	return customersService.getCustomerById(customerId);
 };
