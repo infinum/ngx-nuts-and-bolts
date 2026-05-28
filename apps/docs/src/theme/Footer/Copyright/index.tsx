4ce5d5bf261e8ad8e94f35b3ec249d3e43fbe640
Marin Teskera <marin.teskera@infinum.com>
2026-05-28 12:25:04 +0200

    swizzle docs footer

---
+++ apps/docs/src/theme/Footer/Copyright/index.tsx
@@ -0,0 +1,12 @@
+import React, { type ReactNode } from 'react';
+import type { Props } from '@theme/Footer/Copyright';
+
+export default function FooterCopyright({ copyright }: Props): ReactNode {
+	return (
+		<div
+			className="footer__copyright"
+			// Developer provided the HTML, so assume it's safe.
+			dangerouslySetInnerHTML={{ __html: copyright }}
+		/>
+	);
+}
---
+++ apps/docs/src/theme/Footer/LinkItem/index.tsx
@@ -0,0 +1,17 @@
+import React, { type ReactNode } from 'react';
+import clsx from 'clsx';
+import useBaseUrl from '@docusaurus/useBaseUrl';
+import type { Props } from '@theme/Footer/LinkItem';
+
+export default function FooterLinkItem({ item }: Props): ReactNode {
+	const { to, href, label, prependBaseUrlToHref, className, ...props } = item;
+	const toUrl = useBaseUrl(to);
+	const normalizedHref = useBaseUrl(href, { forcePrependBaseUrl: true });
+	const linkHref = href ? (prependBaseUrlToHref ? normalizedHref : href) : toUrl;
+
+	return (
+		<a className={clsx('footer__link-item', className)} href={linkHref} {...props}>
+			{label}
+		</a>
+	);
+}
